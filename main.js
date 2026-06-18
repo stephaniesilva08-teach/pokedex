b8133aa18c193a1e197adebb97ca0cb0b8a664cb
Michael A. Vega Pabon <dev.michael.vega@gmail.com>
2026-06-18 18:47:54 -0500

    feat: First commit

---
+++ main.js
@@ -0,0 +1,48 @@
+const POKEAPI_URL = "https://pokeapi.co/api/v2";
+const pokemonList = document.getElementById("pokemons");
+
+const loadPokemons = async () => {
+    try {
+        const response = await fetch(`${POKEAPI_URL}/pokemon`).then(response => response.json());
+        response.results.forEach(pokemon => {
+            const option = document.createElement("option");
+            option.textContent = pokemon.name;
+            option.value = pokemon.url;
+            pokemonList.appendChild(option);
+        });
+    } catch (error) {
+        console.error("Error fetching pokemons:", error);
+    }
+}
+
+loadPokemons();
+
+const pokemonSelected = async (pokemonUrl) => {
+    try {
+
+        const response = await fetch(pokemonUrl).then(response => response.json());
+
+        const pokemonImage = document.getElementById("pokemon-image");
+        const pokemonName = document.getElementById("pokemon-name");
+        const pokemonStats = document.getElementById("pokemon-stats");
+
+        pokemonImage.src = response.sprites.front_default;
+        pokemonName.textContent = response.name;
+
+        pokemonStats.innerHTML = "";
+
+        response.stats.forEach(stat => {
+            const li = document.createElement("li");
+            li.textContent = `${stat.stat.name}: ${stat.base_stat}`;
+            pokemonStats.appendChild(li);
+
+        })
+    } catch (error) {
+        console.error("Error fetching pokemon details:", error);
+    }
+}
+// fetch(`${POKEAPI_URL}/pokemon`)
+// .then(response => response.json())
+// .then(data => {
+//     console.log(data);
+// });
